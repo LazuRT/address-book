c5fa617d7d0cf9dc76de6813072b9ed5fefabe7c
Lazuardy Anugrah <lazuardyanugrah@gmail.com>
2026-02-11 16:58:38 +0700

    add contacts array

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 console.log("Test address book");
+
+const contacts = [
+  {
+    id: 1,
+    fullName: "Lazuardy Anugrah",
+    phone: 6281292846785,
+    email: "lazuardyanugrah@lazu.com",
+    Location: "Bogor, Indonesia",
+  },
+  {
+    id: 2,
+    fullName: "Rizky Pratama",
+    phone: 6281376543210,
+    email: "rizky.pratama@gmail.com",
+    Location: "Bandung, Indonesia",
+  },
+  {
+    id: 3,
+    fullName: "Aditya Hari",
+    phone: 6281122334455,
+    email: "adityahari@gmail.com",
+    Location: "Surabaya, Indonesia",
+  },
+];
+
+console.log(contacts);
+for (let i = 0; i < contacts.length; i++) {
+  console.log(contacts[i]);
+}
+
+// contacts.forEach((contact) => {
+//   console.log(contact);
+// });
